907dd1da47a8895ef00e517e0e05b24c50269036
Johan Martinsson <j.martinsson@bee-buzziness.com>
2018-12-10 12:14:23 +0100

    make multiline values work using csv-parse

--- retours/to-html.spec.ts
+++ retours/to-html.spec.ts
@@ -1,13 +1,59 @@
 import {expect} from 'chai'
 import {toHtml} from "./to-html"
+import parse from 'csv-parse/lib/sync'
+import {readFile} from "fs-extra"
 
 describe('to-html', () => {
     it('tranforms the thing to html', async () => {
-        const html = await toHtml(",,,Interet\n" +
+        const html = await toHtml("Nom,Entreprise,Commentaire,Interet\n" +
             "Karim Sanglan ,Orange,Tu dois le faire,5\n" +
             "Eduardo,Thales,The best workshop I have ever taken in recent years of counseling,5\n")
         expect(html).eq("Tu dois le faire\n<p>Karim Sanglan - Orange</p>\n" +
             "The best workshop I have ever taken in recent years of counseling\n<p>Eduardo - Thales</p>")
+    });
+
+    it('handles quotes in csv', async () => {
+        let multiLineCsv = "Nom,Entreprise,Commentaire,Interet\n" +
+            "Mathieu Chauvin,Orange,\"Que ça vaut le coup...\nJ'aime bien\",4"
+        const html = await toHtml(multiLineCsv)
+
+        let parseResult = parse(multiLineCsv, {columns:true})
+        expect(parseResult).eql([{ Nom: 'Mathieu Chauvin', Entreprise: 'Orange', Commentaire: 'Que ça vaut le coup...\nJ\'aime bien', Interet: '4' }])
+        expect(parseResult.map(line => line.Commentaire)).eql(["Que ça vaut le coup...\nJ\'aime bien"])
+        expect(parseResult.map(line => line.Commentaire.replace("\n", ""))).eql(["Que ça vaut le coup...J\'aime bien"])
+        expect(html).eql("Que ça vaut le coup...<br>J'aime bien\n" +
+            "<p>Mathieu Chauvin - Orange</p>")
+    });
+
+    it('parse', () => {
+        const lines = parse("Nom,Entreprise,Commentaire,Interet\n" +
+            "Karim Sanglan ,Orange,Tu dois le faire,5\n", {columns:true})
+        expect(lines).eql([{ Nom: 'Karim Sanglan ',
+            Entreprise: 'Orange',
+            Commentaire: 'Tu dois le faire',
+            Interet: '5' }])
+    });
+
+    it('', () => {
+        expect("\"toto\ntata\"".replace("\n", "<br>")).eql("\"toto<br>tata\"")
+    });
+
+
+    it('bug in parse csv', () => {
+        expect(parse("author,Message\n" +
+            "Tolkien,\"To all my friends in middle earth,\n" +
+            "I wish you a merry christmas!\"\n" +
+            "JK Rowling,\"To all my friends at Hogwarts\n" +
+            "I wish you a merry christmas!\"", {columns: true})).eql([
+            {author: "Tolkien", Message: "To all my friends in middle earth,\nI wish you a merry christmas!"},
+            {author: "JK Rowling", Message: "To all my friends at Hogwarts\nI wish you a merry christmas!"},
+        ])
+    });
+    it('multi-line in file', async () => {
+        let csvContent = await readFile(__dirname + "/multi-line.csv")
+        expect(await toHtml(csvContent.toString())).eql("Que ça vaut le coup...<br>J'aime bien les choix des katas et la possibilité d'aller plus loin pour les plus rapides\n" +
+            "<p>Mathieu Chauvin - Orange</p>")
+
     });
 
 });
--- retours/to-html.ts
+++ retours/to-html.ts
@@ -1,23 +1,18 @@
 import {readFile} from "fs-extra"
+import parse from 'csv-parse/lib/sync'
 
-export async function doExport() {
-    const csvContent = await readFile(__dirname + '/retours-tdd.csv')
+export async function doExport(csvFullPath = __dirname + '/retours-tdd.csv') {
+    const csvContent = await readFile(csvFullPath)
     console.log(await toHtml(csvContent.toString()))
-    // const lines: string[][] = csvContent.toString().split('\n').map(line => line.split(','))
-
 }
 
 export async function toHtml(csvContent: string) {
-    // const csvContent = await readFile(__dirname + '/retours-tdd.csv')
+    const lins = parse(csvContent, {columns:true})
+    return lins.map((line: any) => {
+        const phrase = line.Commentaire.replace("\n", '<br>' )
+        const name = line.Nom.trim()
+        const company = line.Entreprise
+        return `${phrase}\n<p>${name} - ${company}</p>`
+    }).join('\n')
 
-    const lines: string[][] = csvContent.toString()
-        .split('\n')
-        .filter(l => l.length > 0)
-        .filter(l => l.indexOf(',,,') === -1)
-        .map(line => line.split(','))
-    return lines
-        .map(line => line.map(element => element.trim()))
-        .map(([name, company, phrase]) => {
-            return `${phrase}\n<p>${name} - ${company}</p>`
-        }).join('\n')
 }
